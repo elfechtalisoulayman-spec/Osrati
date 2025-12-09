6cf710f2370605df99ec963a75940144c59be525
elfechtalisoulayman-spec <elfechtalisoulayman@gmail.com>
2025-12-09 20:58:43 +0100

    add main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AppProvider } from './context/AppContext';
+import './index.css'; // سننشئ هذا الملف لاحقاً
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>,
+);
